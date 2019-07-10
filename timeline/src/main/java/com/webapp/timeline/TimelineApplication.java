76bc0fcf93c167e18405715ff132164180a00cdb
Juhyeon <kjh96905@gmail.com>
2019-07-10 13:42:37 +0900

    jdbc-driver Connection

--- timeline/src/main/java/com/webapp/timeline/TimelineApplication.java
+++ timeline/src/main/java/com/webapp/timeline/TimelineApplication.java
@@ -3,11 +3,17 @@ package com.webapp.timeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TimelineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TimelineApplication.class, args);
+        try {
+            SpringApplication.run(TimelineApplication.class, args);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
