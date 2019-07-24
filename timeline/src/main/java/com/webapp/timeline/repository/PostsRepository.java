9380a7ee7f595d6b9969f90e37f7dc5850323309
Juhyeon <kjh96905@gmail.com>
2019-07-24 19:54:50 +0900

    update db of posts : take JSON, alter pk

---
+++ timeline/src/main/java/com/webapp/timeline/repository/PostsRepository.java
@@ -0,0 +1,21 @@
+package com.webapp.timeline.repository;
+
+import com.webapp.timeline.domain.PostId;
+import com.webapp.timeline.domain.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PostsRepository extends JpaRepository<Posts, PostId> {
+
+    /*
+    * save() 레코드 저장 (insert, update)
+
+    *   findOne() primary key로 레코드 한건 찾기
+        findAll()	 전체 레코드 불러오기. 정렬(sort), 페이징(pageable) 가능
+        count()	 레코드 갯수
+        delete()	 레코드 삭제
+     */
+
+
+}
