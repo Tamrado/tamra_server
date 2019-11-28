cc80e8c99f3f5f33d30741212ea9ff36a33259c6
juhyeon96 <kjh96905@gmail.com>
2019-11-28 19:54:01 +0900

    add serializer/ deserializer for Comments

---
+++ timeline/src/main/java/com/webapp/timeline/sns/web/CustomCommentDeserializer.java
@@ -0,0 +1,34 @@
+package com.webapp.timeline.sns.web;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.webapp.timeline.sns.domain.Comments;
+
+import java.io.IOException;
+
+public class CustomCommentDeserializer extends StdDeserializer<Comments> {
+
+    public CustomCommentDeserializer() {
+        this(null);
+    }
+
+    public CustomCommentDeserializer(Class<Comments> t) {
+        super(t);
+    }
+
+    @Override
+    public Comments deserialize(JsonParser parser, DeserializationContext context)
+                                                throws JsonProcessingException, IOException {
+
+        JsonNode node = parser.getCodec().readTree(parser);
+        String content = node.get("content").asText(null);
+
+        return new Comments.Builder()
+                        .content(content)
+                        .build();
+    }
+
+}
