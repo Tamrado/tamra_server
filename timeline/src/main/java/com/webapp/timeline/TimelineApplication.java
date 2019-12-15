7954ddbb736674637455e5f5ad160a5ac4472410
juhyeon96 <kjh96905@gmail.com>
2019-12-15 18:57:37 +0900

    add dependencies for quartz-scheduler and spring batch

--- timeline/src/main/java/com/webapp/timeline/TimelineApplication.java
+++ timeline/src/main/java/com/webapp/timeline/TimelineApplication.java
@@ -1,22 +1,24 @@
 package com.webapp.timeline;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @EntityScan(basePackages = {"com.webapp.timeline"})
-@EnableJpaRepositories(basePackages = {"com.webapp.timeline.membership.repository", "com.webapp.timeline.sns.repository","com.webapp.timeline.follow.repository"})
-@EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
-@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.webapp.timeline.membership.repository",
+                                        "com.webapp.timeline.sns.repository",
+                                        "com.webapp.timeline.follow.repository"})
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 public class TimelineApplication {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml, "
             + "classpath:aws.yml, "
-            + "classpath:jwt.yml";
+            + "classpath:jwt.yml, "
+            + "classpath:scheduler.yml";
 
     public static void main(String[] args) {
         try {
