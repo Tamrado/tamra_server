21e561a1a7e0671bae3f5f85dcb326685ec87e7f
juhyeon96 <kjh96905@gmail.com>
2019-12-21 02:18:43 +0900

    create data-transfer objects for object-oriented response

---
+++ timeline/src/main/java/com/webapp/timeline/sns/dto/ImageResponse.java
@@ -0,0 +1,15 @@
+package com.webapp.timeline.sns.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageResponse {
+    private String original;
+    private String thumbnail;
+}
