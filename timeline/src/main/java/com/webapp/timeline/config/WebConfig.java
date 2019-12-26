e720a08e87d3d9c56270acaac0568bcdf1f5f802
juhyeon96 <kjh96905@gmail.com>
2019-12-26 21:07:40 +0900

    sets web socket and spring event for messaging (alarm) service

--- timeline/src/main/java/com/webapp/timeline/config/WebConfig.java
+++ timeline/src/main/java/com/webapp/timeline/config/WebConfig.java
@@ -1,6 +1,10 @@
 package com.webapp.timeline.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -23,5 +27,20 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public MessageSourceAccessor eventMessageSourceAccessor(MessageSource messageSource) {
+        return new MessageSourceAccessor(eventMessageSource());
+    }
+
+    @Bean
+    public MessageSource eventMessageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:event");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(60);
+
+        return messageSource;
     }
 }
