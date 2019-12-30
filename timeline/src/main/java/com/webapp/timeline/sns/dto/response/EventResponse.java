77baf696ebfe880aea240ecd7c3f7b0db404927e
juhyeon96 <kjh96905@gmail.com>
2019-12-30 23:43:42 +0900

    add dto for event response

---
+++ timeline/src/main/java/com/webapp/timeline/sns/dto/response/EventResponse.java
@@ -0,0 +1,26 @@
+package com.webapp.timeline.sns.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class EventResponse {
+    @JsonProperty("sender")
+    private ProfileResponse sender;
+
+    private String message;
+
+    private String timestamp;
+
+    private String link;
+
+    private boolean isRead;
+}
