b83eaef368025408b3b0e28aeeb9e67332943980
Ubuntu <ubuntu@ip-172-31-39-186.ap-northeast-2.compute.internal>
2020-01-10 23:35:08 +0900

    fix conflict

--- timeline/src/main/java/com/webapp/timeline/config/WebConfig.java
+++ timeline/src/main/java/com/webapp/timeline/config/WebConfig.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
-@EnableAsync
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -27,13 +26,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
-    @Override
-    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(10);
-        taskExecutor.setMaxPoolSize(20);
-        taskExecutor.setQueueCapacity(50);
-	taskExecutor.initialize();
-        configurer.setTaskExecutor(taskExecutor);
-    }
 }
